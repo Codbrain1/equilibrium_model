f522e463780a60fb40e35c6eda553248893a9ff6
mask <meshorkin@mail.ru>
2025-03-22 14:05:12 +0300

    version 1.1(alpha)

--- equilibrium_model/Constants.h
+++ equilibrium_model/Constants.h
@@ -8,14 +8,14 @@ namespace Partcile_Particle_model {
 	const double PI = 3.141592653589793238462643383279502884;
 	//initial conditions:
 	//======================================================================
-	const double N = 5000; //число частиц
+	const double N = 1000; //число частиц
 	const double t_0 = 0, t_1 = 1; //начальное и конеченое время
 	double dt = 0.001; //шаг времени
 	double R_max = 1;//максимальный радиус
 	double M = 1;//общая масса частиц
 	double r_0 = 0; //начадбный радиус
 	double dr = R_max/100; //шаг по радиальной компоненте
-	const double r_alpha =0.0002;
+	const double r_alpha =0.001;
 	const double sigma_0= N/PI;
 
 }
--- equilibrium_model/main.cpp
+++ equilibrium_model/main.cpp
@@ -9,7 +9,7 @@ double random(double beg, double end)
 {
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::uniform_real_distribution<> dist(beg,end);
+	std::uniform_real_distribution<> dist(beg, end);
 	return dist(gen);
 }
 int main()
@@ -17,23 +17,23 @@ int main()
 	namespace PPm = Partcile_Particle_model;
 	std::vector<PPm::Particle> particles(PPm::N); //array particals in model
 	//TODO: add initial conditions for Particals
-	 
-	 
-	
+
+
+
 	//setting the initial conditions
 	// 
 	//====================================================================================================
 	double index = 0;
-	double r_k = PPm::r_0+PPm::dr/2.0;
+	double r_k = PPm::r_0 + PPm::dr / 2.0;
 	int sum = 0;
 	while (r_k <= PPm::R_max)
 	{
 		double sigma_r = PPm::sigma(r_k);
-		int N_k = round(sigma_r * PPm::PI * r_k*PPm::dr * PPm::N);
+		int N_k = round(sigma_r * PPm::PI * r_k * PPm::dr * PPm::N);
 		sum += N_k;
 		double phi = 0;
 		double d_phi = 2 * PPm::PI / N_k;
-		for (int i = index; i < index+N_k; i++)
+		for (int i = index; i < index + N_k; i++)
 		{
 			particles[i].r.x = r_k * cos(phi);
 			particles[i].r.y = r_k * sin(phi);
@@ -50,31 +50,31 @@ int main()
 		vec F_ij;
 		for (size_t j = 0; j < particles.size(); j++)
 		{
-		
+
 			if (i != j)
 			{
 				// TODO: realize impuls of moment
 				F_ij = F(particles[i], particles[j]) + F_ij;
-				particles[i].E += - PPm::G * particles[i].m * particles[j].m / (particles[j].r - particles[i].r).module();
+				particles[i].E -= PPm::G * particles[i].m * particles[j].m / (particles[j].r - particles[i].r).module();
 			}
 		}
 		double v_radial = sqrt(particles[i].r.module() * F_ij.module());
-		particles[i].v.x = v_radial; //? TODO угол поворота исправить
-		particles[i].v.y = 0;
+		particles[i].v.x = v_radial * cos(atan(F_ij.y / F_ij.x)); //? TODO: угол поворота исправить
+		particles[i].v.y = v_radial * sin(atan(F_ij.y / F_ij.x));
 		particles[i].v.z = 0;
 		particles[i].E += particles[i].m * particles[i].v.module_2() / 2.0;
 		particles[i].P = particles[i].v * particles[i].m;
 	}
-	
+
 	// integration of differential equations
 	// 
 	//====================================================================================================
 	int k = 0;
-	std::vector<double> sistem_E((int)(PPm::t_1-PPm::t_0)/PPm::dt, 0);
+	std::vector<double> sistem_E((int)(PPm::t_1 - PPm::t_0) / PPm::dt, 0);
 	std::vector<double> sistem_P((int)(PPm::t_1 - PPm::t_0) / PPm::dt, 0);
 	std::vector<double> sistem_M((int)(PPm::t_1 - PPm::t_0) / PPm::dt, 0);
 
-	for (double t = PPm::t_0; t < PPm::t_1; t+=PPm::dt)
+	for (double t = PPm::t_0; t < PPm::t_1; t += PPm::dt)
 	{
 		for (size_t i = 0; i < particles.size(); i++)
 		{
@@ -85,27 +85,28 @@ int main()
 					// TODO: realize impuls of moment
 					particles[i].v = F(particles[i], particles[j]) * PPm::dt + particles[i].v;
 					particles[i].r = particles[i].r + particles[i].v * PPm::dt;
-					particles[i].E += particles[i].m * particles[i].v.module_2() / 2.0 - PPm::G * particles[i].m * particles[j].m / (particles[j].r - particles[i].r).module();
-					particles[i].P = particles[i].P +  particles[i].v*particles[i].m;
+					particles[i].E += particles[i].m * particles[i].v.module_2() / 2.0 + PPm::G * particles[i].m * particles[j].m / (particles[j].r - particles[i].r).module();
+					particles[i].P = particles[i].P + particles[i].v * particles[i].m;
 				}
 			}
 
 			sistem_E[k] += particles[i].E;
 			sistem_P[k] = sistem_P[k] + particles[i].P.module();
 			sistem_M[k] = sistem_M[k] + particles[i].M.module();
-			
+
 		}
 		for (size_t i = 0; i < particles.size(); i++)
 		{
-			particles[i].E = 0; 
-			particles[i].P = vec(0,0,0);
+			particles[i].E = 0;
+			particles[i].P = vec(0, 0, 0);
 		}
-		std::cout << k<<std::endl;
+		std::cout << k;
+		std::cout << " E= " << sistem_E[k] << std::endl;
 		k++;
 	}
-	for (int i=0;i<sistem_E.size();i++)
+	for (int i = 0; i < sistem_E.size(); i++)
 	{
-		std::cout << "t" << i << ": E = " << sistem_E[i] << " P = " <<sistem_P[i]<<" M = "<<sistem_M[i]<<std::endl;
+		std::cout << "t" << i << ": E = " << sistem_E[i] << " P = " << sistem_P[i] << " M = " << sistem_M[i] << std::endl;
 	}
 	//====================================================================================================
 
