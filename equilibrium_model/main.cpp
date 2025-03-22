1089f74aa3e6e724244ab58505a824d53ca9b7b9
mask <meshorkin@mail.ru>
2025-03-22 21:03:32 +0300

    f

--- equilibrium_model/main.cpp
+++ equilibrium_model/main.cpp
@@ -104,7 +104,7 @@ int main()
 		//std::cout << " E= " << sistem_E[k] << std::endl;
 		k++;
 	}
-	std::ofstream conversation_laws("measurements.txt");
+	std::ofstream conversation_laws("C:\\Users\\mesho\\Desktop\\научка_2025_весна\\программная_реализация_Равновесная_Модель\\визуальзация измерений\\measurements.txt");
 	for (int i = 0; i < sistem_E.size(); i++)
 	{
 		conversation_laws<< i <<" "<< sistem_E[i] << " " << sistem_P[i] << " " << sistem_M[i] << std::endl;
